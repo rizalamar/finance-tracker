d015dcc2c480311a218dfa853646d2199873e3ba
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-29 14:41:00 +0700

    update: add error constraint message

--- src/main/java/com/rizalamar/financetracker/model/RegisterUserRequest.java
+++ src/main/java/com/rizalamar/financetracker/model/RegisterUserRequest.java
@@ -1,6 +1,7 @@
 package com.rizalamar.financetracker.model;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,15 +14,17 @@ import lombok.NoArgsConstructor;
 @Builder
 public class RegisterUserRequest {
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3-20 characters")
+    @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "Usernames can only contain letters and numbers")
     private String username;
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 100, message = "Password must be at least 8 characters")
+    @Pattern(regexp = ".*\\d.*", message = "Password must contain at least one number")
     private String password;
 
-    @NotBlank
-    @Size(max = 100)
+    @NotBlank(message = "Name is required")
+    @Size(max = 100, message = "Maximum name is 100 characters")
     private String name;
 }
