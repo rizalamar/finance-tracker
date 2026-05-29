1cecd380c0223cc7021a78d28012e74f7fef89a5
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-29 14:02:24 +0700

    feat: add global handling

---
+++ src/main/java/com/rizalamar/financetracker/controller/ErrorController.java
@@ -0,0 +1,25 @@
+package com.rizalamar.financetracker.controller;
+
+import com.rizalamar.financetracker.model.WebResponse;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class ErrorController {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<WebResponse<String>> constrainViolationException(ConstraintViolationException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(WebResponse.<String>builder().message(exception.getMessage()).build());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<WebResponse<String>> responseStatusException(ResponseStatusException exception) {
+        return ResponseEntity.status(exception.getStatusCode())
+                .body(WebResponse.<String>builder().message(exception.getMessage()).build());
+    }
+}
