61442f17dad958a888c4faa973119e28f05fb1c9
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-31 12:03:02 +0700

    update: wallet repository by adding new find user by user and id

--- src/main/java/com/rizalamar/financetracker/repository/WalletRepository.java
+++ src/main/java/com/rizalamar/financetracker/repository/WalletRepository.java
@@ -1,11 +1,14 @@
 package com.rizalamar.financetracker.repository;
 
+import com.rizalamar.financetracker.entity.User;
 import com.rizalamar.financetracker.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
+    Optional<Wallet> findFirstByUserAndId(User user, UUID id);
 }
